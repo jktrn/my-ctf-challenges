884ea25c873d399e870b37dfa0d6160ee6947328
Jason <123jasont@gmail.com>
2023-08-06 02:48:03 -0700

    refactor: dev repo structure

---
+++ challenge/frontend/Assets/Scripts/GachaManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using RequestClasses;
+
+public class GachaManager : MonoBehaviour
+{
+    public UIManager uiManager;
+    public GachaResponse lastGachaResponse;
+
+    private GameState gameState;
+
+    void Start()
+    {
+        gameState = FindObjectOfType<GameState>();
+    }
+
+    public void OnPullButtonClick(int cost, int numPulls)
+    {
+        if (gameState.crystals < cost)
+        {
+            return;
+        }
+
+        StartCoroutine(SendGachaRequest(numPulls));
+    }
+
+    public IEnumerator SendGachaRequest(int numPulls)
+    {
+        GachaRequest gachaRequest = new GachaRequest(gameState.crystals, gameState.pulls, numPulls);
+        string json = JsonUtility.ToJson(gachaRequest);
+
+        using (UnityWebRequest request = CreateGachaWebRequest(json))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                HandleGachaResponse(request.downloadHandler.text, numPulls);
+                GachaResponse response = JsonUtility.FromJson<GachaResponse>(
+                    request.downloadHandler.text
+                );
+                StartCoroutine(uiManager.DisplaySplashArt(response.characters));
+            }
+            else
+            {
+                uiManager.GenericModalHandler(
+                    uiManager.failedConnectionModal,
+                    uiManager.failedConnectionModalCloseButton
+                );
+                AudioController.Instance.PlaySFX("Open");
+            }
+        }
+    }
+
+    UnityWebRequest CreateGachaWebRequest(string json)
+    {
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+
+        UnityWebRequest request = new UnityWebRequest("http://localhost:3000/gacha", "POST");
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("User-Agent", "SekaiCTF");
+
+        return request;
+    }
+
+    void HandleGachaResponse(string responseText, int numPulls)
+    {
+        GachaResponse response = JsonUtility.FromJson<GachaResponse>(responseText);
+        lastGachaResponse = response;
+
+        gameState.SpendCrystals(numPulls);
+        uiManager.UpdateUI();
+        uiManager.splashArtCanvas.gameObject.SetActive(true);
+        AudioController.Instance.FadeMusic("Pulling");
+    }
+}
